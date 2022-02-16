4e075bbded58eed9541d27684fe8497b0edaa53d
Rakesh-N-Tashildar <tashildar563@protonmail.com>
2022-02-16 09:00:42 +0530

    initial commit

---
+++ TODOAPP/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTodoDialogComponent } from './edit-todo-dialog/edit-todo-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+
+
+import {MatDialogModule} from '@angular/material/dialog';
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    EditTodoDialogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ TODOAPP/src/app/shared/data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Todo } from './todo.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+todos: Todo[] = [
+  new Todo ('this is a test'),
+  new Todo ( 'Lorem ipsum llajsdlkfja ldfjalkjs lfkdja slfjal')
+]
+  constructor() { }
+
+  getAllTodos (){
+    return this.todos
+  }
+  addTodo(todo: Todo){
+    this.todos.push(todo)
+  }
+  updateTodo(index: number, updatedTodo: Todo){
+    this.todos[index]=updatedTodo
+  }
+  deletTodo(index:number){
+    this.todos.splice(index, 1)
+  }
+}
